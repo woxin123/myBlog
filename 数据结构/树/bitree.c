212e9af69613a9105e9605e49f5d51612ba50e4c
孟晨 <1293141942@qq.com>
2017-12-28 15:59:14 +0800

    二叉树的遍历的递归算法

---
+++ 数据结构/树/bitree.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include "bitree.h"
+
+// 前序遍历
+void PreOrderTraverse(BiTNode T) {
+    if (T == NULL)
+        return;
+    printf("%c", T->element);
+    PreOrderTraverse(T->lChild);
+    PreOrderTraverse(T->rChild);
+}
+
+// 中序遍历
+void InOrderTraverse(BiTree T) {
+    if (T == NULL)
+        return;
+    // 先左子树
+    InOrderTraverse(T->lChild);
+    // 然后访问
+    printf("%c", T->element);
+    // 最后右子树
+    InOrderTraverse(T->rChild);
+}
+
+void PostOrderTraverse(BiTree T) {
+    if (T == NULL)
+        return;
+    // 后序遍历左子树
+    PostOrderTraverse(T->lChild);
+    // 后序遍历右子树
+    PostOrderTraverse(T->rChild);
+    // 访问根节点
+    printf("%c", T->element);
+}
---
+++ 数据结构/树/bitree.h
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+typedef char ElementType;
+typedef struct BiTNode {
+    ElementType element;
+    struct BiTNode * lChild;    // 左孩子
+    struct BiTNode * rChild;    // 有孩子
+} BiTNode, *BiTree;
+
+// 二叉树的前序遍历
+void PreOrderTraverse(BiTNode T);
+// 二叉树的中序遍历
+void InOrderTraverse(BiTree T);
+// 二叉树的后序遍历
+void PostOrderTraverse(BiTree T);
